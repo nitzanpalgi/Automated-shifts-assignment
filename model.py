4873484a8a214221942fe2a2620eb27e0ceb9197
nitzanpalgi <nitzanpal@gmail.com>
2021-02-25 14:59:11 +0200

    /

--- model.py
+++ model.py
@@ -9,14 +9,19 @@ def init_constraints(tasks, operators):
 
     mat = add_vars(shifts_model, tasks, operators)
 
-    # shifts_model += xsum() 
+    # shifts_model += xsum()
+
+    # all tasks are assigned
+    for j, task in enumerate(tasks):
+        shifts_model += xsum(mat[i][j] for i, operators in enumerate(operators)) == 1
+        if is_shift_compatible(operators, task), f'task({task})'
 
 
 def add_vars(shifts_model, tasks, operators):
-    [shifts_model.add_var(f'x({i},{j})')
-     for j, task in enumerate(tasks)
-     for i, operator in enumerate(operators)
-     if is_shift_compatible(operators, task)]
+    return [shifts_model.add_var(f'x({i},{j})')
+            for j, task in enumerate(tasks)
+            for i, operator in enumerate(operators)
+            if is_shift_compatible(operators, task)]
 
 
 def is_shift_compatible(operators, task):
