886862093bb02c8dfeab6998f43137963153ffb3
nitzanpalgi <nitzanpal@gmail.com>
2021-02-25 14:51:28 +0200

    added pipenv

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
-from mip import Model, xsum, BINARY 
+from mip import Model, xsum, BINARY
+
 
 def init_constraints(tasks, operators):
     numTasks = len(tasks)
@@ -10,10 +11,13 @@ def init_constraints(tasks, operators):
 
     # shifts_model += xsum() 
 
+
 def add_vars(shifts_model, tasks, operators):
-    return [ [ 
-        is_shift_compatible(
-            shifts_model.add_var(f'x({i},{j})') 
-        )
-        for j in range(len(tasks)) ] 
-    for i in range(len(operators)) ]
+    [shifts_model.add_var(f'x({i},{j})')
+     for j, task in enumerate(tasks)
+     for i, operator in enumerate(operators)
+     if is_shift_compatible(operators, task)]
+
+
+def is_shift_compatible(operators, task):
+    return True
