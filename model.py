ae12426c02092f0c48d3b51424886de9a643dcf4
nitzanpalgi <nitzanpal@gmail.com>
2021-02-25 15:37:39 +0200

    added all tasks are assigned constrains

--- model.py
+++ model.py
@@ -7,22 +7,25 @@ def init_constraints(tasks, operators):
 
     shifts_model = Model()
 
-    mat = add_vars(shifts_model, tasks, operators)
+    mat = add_vars(shifts_model, operators, tasks)
 
     # shifts_model += xsum()
 
-    # all tasks are assigned
-    for j, task in enumerate(tasks):
-        shifts_model += xsum(mat[i][j] for i, operators in enumerate(operators)) == 1
-        if is_shift_compatible(operators, task), f'task({task})'
+    shifts_model += all_tasks_are_assigned_constrains(mat, operators, tasks)
 
 
-def add_vars(shifts_model, tasks, operators):
+def add_vars(shifts_model, operators, tasks):
     return [shifts_model.add_var(f'x({i},{j})')
             for j, task in enumerate(tasks)
             for i, operator in enumerate(operators)
-            if is_shift_compatible(operators, task)]
+            if is_operator_qualified(operators, task)]
 
 
-def is_shift_compatible(operators, task):
+def all_tasks_are_assigned_constrains(mat, operators, tasks):
+    return [(xsum(mat[i][j] for i, operators in enumerate(operators)
+             if is_operator_qualified(operators, task)) == 1,
+             f'task({task})') for j, task in enumerate(tasks)]
+
+
+def is_operator_qualified(operators, task):
     return True
