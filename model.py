217db35be7a55de30a15fd289405e9453a4654bf
nitzanpalgi <nitzanpal@gmail.com>
2021-02-25 16:21:06 +0200

    added overlapping constrains

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 from mip import Model, xsum, BINARY
+import datetime, calendar
 
 
 def init_constraints(tasks, operators):
@@ -15,17 +16,40 @@ def init_constraints(tasks, operators):
 
 
 def add_vars(shifts_model, operators, tasks):
-    return [shifts_model.add_var(f'x({i},{j})', var_type=BINARY)
-            for j, task in enumerate(tasks)
-            for i, operator in enumerate(operators)
+    return [shifts_model.add_var(f'x({operator.id},{task.id})', var_type=BINARY)
+            for task in tasks
+            for operator in operators
             if is_operator_qualified(operators, task)]
 
 
 def all_tasks_are_assigned_constrains(mat, operators, tasks):
-    return [(xsum(mat[i][j] for i, operators in enumerate(operators)
-             if is_operator_qualified(operators, task)) == 1,
-             f'task({task})') for j, task in enumerate(tasks)]
+    return [(xsum(mat[operator.id][task.id] for operator in operators
+                  if is_operator_qualified(operator, task)) == 1, f'task({task})') for task in tasks]
 
 
-def is_operator_qualified(operators, task):
+def task_overlap_constrains(mat, operators, tasks):
+    constrains = []
+    for day in get_days_in_current_month():
+        for operator in operators:
+            constrains += xsum(
+                mat[operator.id][task.id] for task in tasks
+                if (task.start_time <= day <= task.end_time and
+                    is_operator_qualified(operator,task))) <= 1, \
+                          f'day-({day}) operator-({operator})'
+
+    return constrains
+
+
+def is_operator_qualified(operator, task):
     return True
+
+
+def get_days_in_current_month():
+    current_date = datetime.datetime.now()
+    year, month = current_date.year, current_date.month
+    num_days = calendar.monthrange(year, month)[1]
+    return [datetime.date(year, month, day) for day in range(1, num_days + 1)]
+
+
+if __name__ == "__main__":
+    get_days_in_current_month()
