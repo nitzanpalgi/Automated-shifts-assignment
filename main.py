8b8aadb51f70e7f947b066b580133e3e92a3a4cf
nitzanpalgi <nitzanpal@gmail.com>
2021-02-28 16:25:16 +0200

    with output

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
 
     shifts_model = init_constraints(tasks, operators)
     shifts_model.optimize()
-    if shifts_model.num_solutions
+    if shifts_model.num_solutions:
         dfs = convert_to_readable_df(shifts_model, "1/3/2021", "31/3/2021", tasks, operators)
         dfs[0].to_excel("./By operator.xlsx")
         # by task
