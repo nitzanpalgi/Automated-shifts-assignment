2ff58dc2ff469998626bfd0a05972580b069c236
nitzanpalgi <nitzanpal@gmail.com>
2021-02-28 12:46:06 +0200

    merged with master

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 from model import *
+from Modules.dataImporter import CSV_importer
+
+DATA_PATH = '../data/DB.xlsx'
 
 if __name__ == "__main__":
+    tasks, operators = CSV_importer(DATA_PATH)
+
     init_constraints()
