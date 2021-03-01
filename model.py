c47c26aca398cb7977078bff5e9998d01f2ee5c1
nitzanpalgi <nitzanpal@gmail.com>
2021-03-01 09:10:12 +0200

    cleaned Dataframe from functions input

--- model.py
+++ model.py
@@ -1,8 +1,6 @@
 from mip import Model, xsum, BINARY, maximize
 from functions import *
 
-from pandas import DataFrame
-
 
 def init_constraints(tasks_df, operators_df):
     shifts_model = Model()
@@ -29,7 +27,7 @@ def get_var_if_qualified(shifts_model, operator, task, operator_id, task_id):
     return 1
 
 
-def add_vars(shifts_model, operators: DataFrame, tasks: DataFrame):
+def add_vars(shifts_model, operators, tasks):
     return [
         {task_id: shifts_model.add_var(f'x({operator_id},{task_id})', var_type=BINARY)
          for task_id, task in tasks
@@ -38,13 +36,13 @@ def add_vars(shifts_model, operators: DataFrame, tasks: DataFrame):
     ]
 
 
-def add_all_tasks_are_assigned_constrains(model, x_mat, operators: DataFrame, tasks: DataFrame):
+def add_all_tasks_are_assigned_constrains(model, x_mat, operators, tasks):
     for task_id, task in tasks:
         model += xsum(x_mat[operator_id][task_id] for operator_id, operator in operators
                       if is_operator_qualified(operator, task)) == 1, f'task({task_id})'
 
 
-def add_task_overlap_constrains(model, x_mat, operators: DataFrame, tasks: DataFrame):
+def add_task_overlap_constrains(model, x_mat, operators, tasks):
     for day in get_days_in_current_month():
         relevant_tasks = [(task_id, task) for task_id, task in tasks if task["start_time"] <= day <= task["end_time"]]
         for operator_id, operator in operators:
@@ -53,7 +51,7 @@ def add_task_overlap_constrains(model, x_mat, operators: DataFrame, tasks: DataF
                 if is_operator_qualified(operator, task)) <= 1, f'overlapping-({operator_id},{day}))'
 
 
-def add_operator_capacity_constraint(model, x_mat, operators: DataFrame, tasks: DataFrame):
+def add_operator_capacity_constraint(model, x_mat, operators, tasks):
     for operator_id, operator in operators:
         model += xsum(
             task["cost"] * x_mat[operator_id][task_id] for task_id, task in tasks if
