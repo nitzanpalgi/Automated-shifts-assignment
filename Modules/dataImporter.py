27a69840b0c4489932769f75b6e77c9159b809fb
DaniRubin <37410225+DaniRubin@users.noreply.github.com>
2021-03-01 13:55:31 +0200

    Added holidays calculations

--- Modules/dataImporter.py
+++ Modules/dataImporter.py
@@ -3,6 +3,7 @@ from datetime import timedelta, datetime
 from functions import get_days_in_current_month
 from numpy import random, sum
 
+
 # Turn tasks list into a task DataFrame
 def format_tasks_list(tasks):
     start_time = [task[0] for task in tasks]
@@ -14,35 +15,42 @@ def format_tasks_list(tasks):
     ids = [task[6] for task in tasks]
 
     return pd.DataFrame({
-        'id': ids, 
-        'start_time': start_time, 
-        'end_time': end_time, 
-        'name': name, 
+        'id': ids,
+        'start_time': start_time,
+        'end_time': end_time,
+        'name': name,
         'cost': cost,
-        'Compatible': Compatible, 
+        'Compatible': Compatible,
         'min_per_month': min_per_month
     })
+
+
+def US_day_to_IL_day(day):
+    return ((day + 1) % 7) + 1
 
 
 # Handle row and add to task_list
 def distribute_tasks_in_day(row_data):
     tasks_in_day = []
-
     for day in get_days_in_current_month():
         if random.rand() <= row_data['probability']:
-            date_time_str = f'{day} {row_data["start-hour"]}'
-            start_time = datetime.strptime(date_time_str, '%Y-%m-%d %H:%M:%S')
-            end_time = start_time + timedelta(hours=row_data['time'])
-            new_task = [start_time, end_time, row_data['name'], row_data['cost'], row_data['Compatible'], 1, row_data['id']]
-            tasks_in_day.append(new_task)
+            if str(US_day_to_IL_day(day.weekday())) in str(row_data['days_in_week']):
+                date_time_str = f'{day} {row_data["start-hour"]}'
+                start_time = datetime.strptime(date_time_str, '%Y-%m-%d %H:%M:%S')
+                end_time = start_time + timedelta(hours=row_data['time'])
+                new_task = [start_time, end_time, row_data['name'], row_data['cost'], row_data['Compatible'],
+                            1, row_data['id']]
+                tasks_in_day.append(new_task)
+            else:
+                # print(f'task {row_data["name"]} cannot be at {day}')
+                pass
 
     return tasks_in_day
 
 
 # Spread tasks in month according to each task data
-def distribute_tasks_in_month(tasks_data): 
+def distribute_tasks_in_month(tasks_data):
     tasks = []
-
     for index, row_data in tasks_data.iterrows():
         tasks += distribute_tasks_in_day(row_data)
 
@@ -60,10 +68,18 @@ def recalculate_operators_capacity(operators, tasks):
 def import_data_from_excel(path):
     tasks_data = pd.read_excel(path, sheet_name="Tasks")
     tasks = distribute_tasks_in_month(tasks_data)
-
+    print(len(tasks))
     tasks_df = format_tasks_list(tasks)
     operators_df = pd.read_excel(path, sheet_name="Operators")
-
     operators_df["MAX"] = recalculate_operators_capacity(operators_df, tasks_df)
 
     return tasks_df, operators_df
+
+
+def main():
+    DB_path = '../DATA/DB.xlsx'
+    import_data_from_excel(DB_path)
+
+
+if __name__ == "__main__":
+    main()
--- main.py
+++ main.py
@@ -12,12 +12,9 @@ if __name__ == "__main__":
     print(f'number of tasks {len(tasks)}')
 
     shifts_model = init_constraints(tasks, operators)
-
     shifts_model.optimize()
-
     print(time.time() - t)
-
     if shifts_model.num_solutions:
         print(f'objective value {shifts_model.objective_value}')
         final_df = convert_to_readable_df(shifts_model, tasks, operators, DATA_PATH)
-        final_df.T.to_excel('./output/dani.xlsx')
+        final_df.T.to_excel('./output/Butzi.xlsx')
