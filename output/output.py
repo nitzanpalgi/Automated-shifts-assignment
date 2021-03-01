ef630ac48a547b20ef2c753ba11e4a7cb5f87cc7
נעה בן ציון <omri157@gmail.com>
2021-03-01 15:28:12 +0200

    color weekend working

--- output/output.py
+++ output/output.py
@@ -4,10 +4,12 @@ from functions import *
 from datetime import timedelta, date
 from openpyxl.reader.excel import load_workbook
 from openpyxl.workbook import Workbook
-from openpyxl.styles import Color, PatternFill,Fill, Font, Border
+from openpyxl.styles import Color, PatternFill, Fill, Font, Border,Alignment
 from openpyxl.cell import Cell
 import datetime
 # dataframe for each operator as row
+
+
 def create_operators_dataframe(operators):
     df = pd.DataFrame(columns=operators.name)
     for date in get_days_in_current_month():
@@ -15,6 +17,8 @@ def create_operators_dataframe(operators):
     return df
 
 # dataframe for each task as a row
+
+
 def create_task_dataframe(DB_path):
     data_df = pd.read_excel(DB_path, sheet_name='Tasks')
     df = pd.DataFrame(columns=data_df.name)
@@ -29,7 +33,6 @@ def convert_to_readable_df(shifts_model, tasks, operators, DB_path):
     tasks_df_multiple = create_task_dataframe(DB_path)
     tasks_df = tasks_df_multiple[0]
     tasks_df_colors = tasks_df_multiple[1]
-
 
     df = pd.DataFrame()
     # needs to get the shifts model
@@ -46,24 +49,26 @@ def convert_to_readable_df(shifts_model, tasks, operators, DB_path):
 
             # if this is sofash write the next day too
             if 'Sofash' in task_name:
-                print("Found sofash at - ", task_start_time)
                 nextDay = tasks['start_time'][cell_y] + timedelta(days=1)
                 next_day_start_time = str(nextDay).split(' ')[0]
                 df.at[operator_name, next_day_start_time] = task_name
 
-    colors_dict= create_colors_dict(tasks_df_colors)
-    df =df.sort_index(1)
-    return df,colors_dict
+    colors_dict = create_colors_dict(tasks_df_colors)
+    df = df.sort_index(1)
+    return df, colors_dict
+
 
 def create_colors_dict(color_df):
     colors_dict_from_df = color_df.to_dict()
-    colors_dict ={}
+    colors_dict = {}
     for i in range(len(colors_dict_from_df["name"])):
-        colors_dict[colors_dict_from_df["name"][i]]=colors_dict_from_df["color"][i][1:]
+        colors_dict[colors_dict_from_df["name"][i]
+                    ] = colors_dict_from_df["color"][i][1:]
     return colors_dict
 
-def color_cells(file_path,color_dict):
-    new_book= Workbook()
+
+def color_cells(file_path, color_dict):
+    new_book = Workbook()
     new_sheet = new_book.active
     book = load_workbook(file_path)
     ws = book.worksheets[0]
@@ -71,26 +76,30 @@ def color_cells(file_path,color_dict):
     max_row = int(ws.max_row)
     for col in ws:
         for cell in col:
-            new_cell=new_sheet.cell(row = cell.row,column=cell.column,value =cell.value)
+            new_cell = new_sheet.cell(
+                row=cell.row, column=cell.column, value=cell.value)
             # color by task
             if cell.value in color_dict:
-                new_cell.fill= PatternFill(start_color= color_dict[cell.value],end_color= color_dict[cell.value],fill_type="solid")
-            #add weekend colors
-            elif datetime.datetime(ws.cell(column=1,row=cell).value).weekday()==4 or datetime.datetime(ws.cell(column=1,row=cell).value).weekday()==5 :
-                new_cell.fill=PatternFill(start_color="e4e4e4" ,end_color="e4e4e4" ,fill_type="solid")
-                new_cell.font = Font(bold=True) 
-            #bold for titles
+                new_cell.fill = PatternFill(
+                    start_color=color_dict[cell.value], end_color=color_dict[cell.value], fill_type="solid")
+            # add weekend colors
+            elif cell.row > 1 and (datetime.datetime.strptime((ws.cell(cell.row, 1,).value), '%Y-%m-%d').weekday() == 4 or datetime.datetime.strptime((ws.cell(cell.row, 1,).value), '%Y-%m-%d').weekday()==5):
+                new_cell.fill = PatternFill(
+                    start_color="e4e4e4", end_color="e4e4e4", fill_type="solid")
+                new_cell.font = Font(bold=True)
+            # bold for titles
             else:
-                new_cell.font = Font(bold=True) 
+                new_cell.font = Font(bold=True)
+
     return new_book.save('./output/Butzi.xlsx')
 
-    
 
 def main():
     shifts_model = {}
     DB_path = '../DATA/DB.xlsx'
     tasks, operators = dataImporter.import_data_from_excel(DB_path)
-    operators_df, tasks_df = convert_to_readable_df(shifts_model, tasks, operators, DB_path)
+    operators_df, tasks_df = convert_to_readable_df(
+        shifts_model, tasks, operators, DB_path)
     operators_df.to_excel("./operators_data.xlsx")
     tasks_df.to_excel("./tasks_data.xlsx")
 
