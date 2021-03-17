a6cf8f399beaee927725a69fa84c310d79cadbf5
nitzanpalgi <nitzanpal@gmail.com>
2021-03-17 13:57:53 +0200

    hotfixes to variety

--- model.py
+++ model.py
@@ -121,7 +121,7 @@ def add_variety_constraint(model, x_mat, slack_variables, operators, tasks):
         target_number_of_tasks = operator["MAX"] / np.mean([task["cost"] for task in relevant_tasks])
         task_freq_modifier = sum(task['freq'] for task in relevant_tasks)
         for taskType in relevant_tasks:
-            target_number_of_tasks_per_type = target_number_of_tasks * taskType['freq'] * task_freq_modifier
+            target_number_of_tasks_per_type = target_number_of_tasks * taskType['freq'] / task_freq_modifier
             model += xsum(x_mat[operator_id][task_id] for task_id, task in tasks if
                           (is_operator_capable(operator, task) and task["type"] == taskType['type'])) \
                      >= target_number_of_tasks_per_type - 1 - slack_variables[operator_id] \
