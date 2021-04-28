152ed74f9d74bafc7c7328b2427903cea9f768c9
nitzanpalgi <nitzanpal@gmail.com>
2021-04-28 14:21:39 +0300

    weekend capacity is included in total capacity

--- constants.py
+++ constants.py
@@ -3,7 +3,7 @@ MIN_CAPACITY_NOT_WEEKEND = 2
 MAX_CAPACITY_NOT_WEEKEND = 1.5
 
 # add_weekly_capactiy_constraint
-MAX_WEEKLY_CAPACITY = 0.5
+MAX_WEEKLY_CAPACITY = 0.2
 
 # add_operator_capacity_constraint_nights
 MAX_NIGHT_CAPACITY = 1.2
